16f142708cf5638c2333a5f8dab2d2c48df95032
wandji20 <wbertrand10@gmail.com>
2021-02-18 01:21:38 +0100

    Add create action and new view for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
   def index
-    
+
+  end
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path,
+      notice: " #{@user.name}, You Have Successfully Registered as #{@user.username}"
+    else
+      render 'new',
+      alert: 'Something went wrong, Please Try Again'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :password)
   end
 end
